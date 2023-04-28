f58a6db3532d1b60ee56d7575813890b61f0a3a8
Miguel Ulloa <miguel.bmangelulloa13@gmail.com>
2023-04-28 17:52:36 -0600

    Clase-3 Agregar modelos

---
+++ YouTubeClone/YouTubeClone/Model/PlaylistModel.swift
@@ -0,0 +1,8 @@
+//
+//  PlaylistModel.swift
+//  YouTubeClone
+//
+//  Created by Miguel Angel Bric Ulloa on 27/04/23.
+//
+
+import Foundation
---
+++ YouTubeClone/YouTubeClone/Model/VideoModel.swift
@@ -0,0 +1,8 @@
+//
+//  VideoModel.swift
+//  YouTubeClone
+//
+//  Created by Miguel Angel Bric Ulloa on 27/04/23.
+//
+
+import Foundation
